16abaef5d9574c58c03dcdd02157d831779ef8b4
samjhooker <samjhooker@gmail.com>
2014-12-24 17:34:17 +1300

    basic UI

---
+++ LocationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LocationViewController.swift
+//  cyclotron
+//
+//  Created by Samuel Hooker on 24/12/14.
+//  Copyright (c) 2014 Jocus Interactive. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
